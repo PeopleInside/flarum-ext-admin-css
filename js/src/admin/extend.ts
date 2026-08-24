9edd8175a84d356d8a4bd1e0ef5054aa9bcbef03
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-08-24 20:21:23 +0200

    Add custom CSS setting to admin panel

---
+++ js/src/admin/extend.ts
@@ -0,0 +1,12 @@
+import Extend from 'flarum/common/extenders';
+import app from 'flarum/admin/app';
+
+export default [
+    new Extend.Admin()
+        .setting(() => ({
+            setting: 'peopleinside-admin-css.custom_css',
+            label: app.translator.trans('peopleinside-admin-css.admin.custom_css_label', {}, true),
+            help: app.translator.trans('peopleinside-admin-css.admin.custom_css_help', {}, true),
+            type: 'textarea',
+        }), 10)
+];
