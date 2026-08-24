0c2fe797ee35c214a6686da63029c9ef793530ab
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-08-24 20:42:14 +0200

    Refactor admin CSS translation addition

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,10 +1,14 @@
 import app from 'flarum/admin/app';
+import { default as extend } from './extend';
+
+// Export the extend array - THIS IS CRITICAL!
+export { extend };
 
 app.initializers.add('peopleinside-admin-css', () => {
-    app.translator.add('peopleinside-admin-css', {
-        admin: {
-            custom_css_label: 'Custom Admin CSS',
-            custom_css_help: 'Insert custom CSS rules to style the Flarum administration backend. Use with caution, as invalid CSS may affect the admin interface layout.',
-        }
-    });
+  app.translator.add('peopleinside-admin-css', {
+    admin: {
+      custom_css_label: 'Custom Admin CSS',
+      custom_css_help: 'Insert custom CSS rules to style the Flarum administration backend. Use with caution, as invalid CSS may affect the admin interface layout.',
+    }
+  });
 });
