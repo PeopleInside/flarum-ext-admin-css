441bb6ce0bc6afbc0223956785d5d918cb620105
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-08-24 20:43:02 +0200

    Refactor Extend.Admin setting syntax

--- js/src/admin/extend.ts
+++ js/src/admin/extend.ts
@@ -2,11 +2,11 @@ import Extend from 'flarum/common/extenders';
 import app from 'flarum/admin/app';
 
 export default [
-    new Extend.Admin()
-        .setting(() => ({
-            setting: 'peopleinside-admin-css.custom_css',
-            label: app.translator.trans('peopleinside-admin-css.admin.custom_css_label', {}, true),
-            help: app.translator.trans('peopleinside-admin-css.admin.custom_css_help', {}, true),
-            type: 'textarea',
-        }), 10)
+  new Extend.Admin()
+    .setting(() => ({
+      setting: 'peopleinside-admin-css.custom_css',
+      label: app.translator.trans('peopleinside-admin-css.admin.custom_css_label', {}, true),
+      help: app.translator.trans('peopleinside-admin-css.admin.custom_css_help', {}, true),
+      type: 'textarea',
+    }), 10)
 ];
